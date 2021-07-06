0db6e1634cad2f171b6c5a16c1d82ff87cb4e595
Vaishakh <vaishakh.nargund1999@gmail.com>
2021-07-06 16:23:18 +0530

    task done

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from typing import Optional
 
-from fastapi import FastAPI
-from .import schemas
+from fastapi import FastAPI,Depends
+import schemas,models
+from database import engine,SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 
+models.Base.metadata.create_all(bind=engine)
 
+def get_db(): 
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
@@ -14,10 +23,21 @@ def read_root():
 
 
 @app.post("/employee")
-def create_employee(new_emp:schems.Employee):
+def create_employee(request:schemas.Employee,db: Session =Depends(get_db)):
+    new_emp=models.Employee(employeeName=request.employeeName,employeeAge=request.employeeAge,employeeAddress=request.employeeAddress)
+    db.add(new_emp)
+    db.commit()
+    db.refresh(new_emp)
+    return new_emp
+
+@app.get("/employee/all")
+def all_employee(db: Session =Depends(get_db)):
+    emps=db.query(models.Employee).all()
+    return emps
+
+@app.get("/employee/{id}")
+def show_employee(id,db: Session =Depends(get_db)):
+    emp=db.query(models.Employee).filter(models.Employee.id==id).first()
+    return emp
     
 
-
-@app.get("/employee/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
